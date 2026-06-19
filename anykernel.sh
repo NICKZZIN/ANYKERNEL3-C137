1933ab858952fa19eae135ea14fb69f2714276a1
NICKZZIN <elmitoso190@hotmail.com>
2026-06-19 07:54:30 -0300

    Update kernel installation scripts and tools

--- anykernel.sh
+++ anykernel.sh
@@ -2,107 +2,70 @@
 ## osm0sis @ xda-developers
 
 ### AnyKernel setup
-# begin properties
+# global properties
 properties() { '
 kernel.string=
 do.devicecheck=1
-do.modules=0
-do.systemless=1
 do.cleanup=1
-do.cleanuponabort=0
 device.name1=moonstone
-device.name2=stone
-device.name3=sunstone
+device.name2=sunstone
+device.name3=stone
 device.name4=gemstone
-supported.versions=15.0-16.0
-supported.patchlevels=
 '; } # end properties
 
-### AnyKernel install
-# begin attributes
-attributes() {
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-} # end attributes
-
-
-## boot shell variables
-block=boot;
-is_slot_device=1;
-ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
-kernel_image=Image
+# boot shell variables
+BLOCK=boot;
+IS_SLOT_DEVICE=auto;
+NO_BLOCK_DISPLAY=1;
+RAMDISK_COMPRESSION=auto;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
-. tools/ak3-core.sh && attributes;
+. tools/ak3-core.sh;
+. tools/ak3-custom.sh;
+
+DEVICE=$(getprop ro.product.device)
+MARKET_NAME=$(getprop ro.product.vendor.marketname)
+
+[ -z "$DEVICE" ] && DEVICE="unknown"
+[ -z "$MARKET_NAME" ] && MARKET_NAME="Unknown Device"
+
+
+ui_print " "
+ui_print "    CCCCCCCCCCC   111111   3333333333  7777777777777777"
+ui_print "  CC:::::::::::C 1:::::1  3::::::::::337::::::::::::::7"
+ui_print " C::::::::::::::1::::::1  3333333::::::7::::::::::::::7"
+ui_print "C:::::CCCCC:::::111::::1         3:::::777777777::::::7"
+ui_print "C:::::C    CCCCC   1:::1      3333:::::3       7:::::7 "
+ui_print "C:::::C            1:::1      3:::::::3       7:::::7  "
+ui_print "C:::::C            1:::1      3333::::3      7:::::7   "
+ui_print "C:::::C            1:::l         3:::3      7:::::7    "
+ui_print "C:::::C     CCCCC  1:::l         3::::3    7:::::7     "
+ui_print "C:::::CCCCCC::::1111:::l1133333333:::::3  7:::::7      "
+ui_print " C::::::::::::::1:::::::::3::::::::::::3 7:::::7       "
+ui_print "  CCCCCCCCCCCCCC1111111111333333333333337777777        "
+ui_print " "
+ui_print "      C137 Kernel for $DEVICE By Nick C137"
+ui_print "               be kind to yourself"
+ui_print " "
+
 
 # boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-#backup_file init.tuna.rc;
-#insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-#append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-#backup_file fstab.tuna;
-#patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-#patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-#patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-#append_file fstab.tuna "usbdisk" fstab;
-
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+split_boot;
+flash_boot;
 ## end boot install
 
+# dtbo install
+erase_dtbo
+## end dtbo install
 
-## init_boot shell variables
-#block=init_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
-
-# reset for init_boot patching
-#reset_ak;
-
-# init_boot install
-#dump_boot; # unpack ramdisk since it is the new first stage init ramdisk where overlay.d must go
-
-#write_boot;
-## end init_boot install
-
-
-## vendor_kernel_boot shell variables
-#block=vendor_kernel_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
-
-# reset for vendor_kernel_boot patching
-#reset_ak;
-
-# vendor_kernel_boot install
-#split_boot; # skip unpack/repack ramdisk, e.g. for dtb on devices with hdr v4 and vendor_kernel_boot
-
-#flash_boot;
-## end vendor_kernel_boot install
-
-
-## vendor_boot shell variables
-#block=vendor_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
+# vendor_boot shell variables
+BLOCK=vendor_boot;
 
 # reset for vendor_boot patching
-#reset_ak;
+reset_ak;
 
 # vendor_boot install
-#dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
-
-#write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
+split_boot;
+check_patches;
+flash_boot;
 ## end vendor_boot install
-
